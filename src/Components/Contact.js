d400fa3b82df626754b791c47c416ba66fea0dc1
mjlynch123 <mjlynch35@yahoo.com>
2024-02-23 22:01:33 -0600

    updates

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,24 +5,35 @@ export default function Contact() {
     return (
         <div className="contact-container">
             <form className="contact-form">
-                <h1>Contact</h1>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" name="name" />
+                <div className="contact-left">
+                    <div className="contact-info">
+                        <h1>Contact Information</h1>
+                        <p>For any inquiries, please fill out this form and I will get back to as soon as possible.</p>
+                    </div>
                 </div>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" />
+                <div className="contact-right">
+                    <div>
+                        <label htmlFor="name">Name:</label>
+                        <br />
+                        <input type="text" id="name" name="name" />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email:</label>
+                        <br />
+                        <input type="email" id="email" name="email" />
+                    </div>
+                    <div>
+                        <label htmlFor="phone">Phone:</label>
+                        <br />
+                        <input type="text" id="phone" name="phone" />
+                    </div>
+                    <div>
+                        <label htmlFor="message">Message:</label>
+                        <br />
+                        <textarea id="message" name="message" placeholder="Write your message" />
+                    </div>
+                    <button type="submit">Send</button>
                 </div>
-                <div>
-                    <label htmlFor="phone">Email:</label>
-                    <input type="text" id="phone" name="phone" />
-                </div>
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <textarea id="message" name="message" />
-                </div>
-                <button type="submit">Send</button>
             </form>
         </div>
     );
